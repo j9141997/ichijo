90be565fd169f68cfce2d3823af7478e9a41e1ca
j9141997 <j9141997@gmail.com>
2021-07-07 22:50:22 +0900

    feat: color, palette追加

--- styles/variables.ts
+++ styles/variables.ts
@@ -1,7 +1,12 @@
 export const Space = 6;
 
-export const Color = {
-  BORDER: {
-    primary: '#eaeaea',
-  },
+const Palette = {
+  MONO_0: '#000000',
+  MONO_10: '#333333',
+  MONO_20: '#666666',
+  MONO_30: '#999999',
+  MONO_40: '#CCCCCC',
+  MONO_50: '#FFFFFF',
 } as const;
+
+export const Color = {} as const;
