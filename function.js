a6a7f2f390f9ff79c2400f81dc8bb82137121757
Junki Yoshida <50947613+j9141997@users.noreply.github.com>
2021-07-08 23:03:02 +0900

    chore: CI周り (#17)

--- function.js
+++ function.js
@@ -1,14 +1,22 @@
-const {https} = require('firebase-functions');
+const {https, config} = require('firebase-functions');
 const {default: next} = require('next');
+const express = require('express');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
-const server = next({
+const routes = require('next-routes');
+const basicAuth = require('basic-auth-connect');
+
+const {username, password} = config().someservice;
+
+const nextApp = next({
   dev: isDev,
   conf: {distDir: '.next'},
 });
+const server = express();
 
-const nextjsHandle = server.getRequestHandler();
-exports.nextServer = https.onRequest((req, res) => {
-  return server.prepare().then(() => nextjsHandle(req, res));
-});
+const nextjsHandle = routes().getRequestHandler(nextApp);
+server.use(basicAuth(username, password));
+server.get('*', (req, res) => nextjsHandle(req, res));
+
+exports.nextServer = https.onRequest(server);
