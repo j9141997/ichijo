0e03e8122baa6714f9e49e91734a8dc00a54c0e9
j9141997 <j9141997@gmail.com>
2021-07-30 12:44:19 +0900

    fix

--- components/common/NavBar/NavBar.tsx
+++ components/common/NavBar/NavBar.tsx
@@ -1,14 +1,16 @@
 import {css} from '@emotion/react';
 import Link from 'next/link';
+import {Space} from '../../../styles/variables';
 
 const wrapper = css`
   display: flex;
   align-items: center;
   max-width: 1080px;
   height: 48px;
-  padding: 0 8px;
+  padding: 0 ${Space}px;
   margin: 0 auto;
 `;
+
 export const NavBar: React.VFC = () => {
   return (
     <header css={wrapper}>
