8f61df2fd0bbe5f62884995b64bddf8f2e261fff
j9141997 <j9141997@gmail.com>
2021-08-12 11:51:19 +0900

    feat: logo

---
+++ components/common/Logo/Logo.stories.tsx
@@ -0,0 +1,9 @@
+import {Logo} from '.';
+
+export const showNav = () => <Logo />;
+
+const Config = {
+  title: 'Logo',
+};
+
+export default Config;
--- components/common/NavBar/NavBar.tsx
+++ components/common/NavBar/NavBar.tsx
@@ -1,20 +1,23 @@
 import {css} from '@emotion/react';
 import Link from 'next/link';
 import {Space} from '../../../styles/variables';
+import {Logo} from '../Logo/Logo';
 
 const wrapper = css`
   display: flex;
   align-items: center;
   max-width: 1080px;
   height: 48px;
-  padding: 0 ${Space}px;
+  padding: 0 ${Space * 2}px;
   margin: 0 auto;
 `;
 
 export const NavBar: React.VFC = () => {
   return (
     <header css={wrapper}>
-      <Link href="/">PRODUCT X</Link>
+      <Link href="/" passHref>
+        <Logo />
+      </Link>
     </header>
   );
 };
